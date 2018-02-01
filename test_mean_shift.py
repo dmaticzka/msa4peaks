2258e126d629ad8941566a548e4985a8c27cb2b1
fejjaz <fayyaz.hussain@gmail.com>
2018-02-01 13:23:54 +0100

    pytest file

--- test_mean_shift.py
+++ test_mean_shift.py
@@ -1,6 +1,8 @@
 from mean_shift import Mean_Shift
 
+
 def test_main():
     a = Mean_Shift(3)
     x = [1, 2, 3, 4, 5, 9, 10, 11, 12, 13, 20, 21, 22, 23, 24]
-    assert a.peak(x) == {0:3, 1:11, 2:22}
+    assert a.peak(x) == {0: 3, 1: 11, 2: 22}
+
